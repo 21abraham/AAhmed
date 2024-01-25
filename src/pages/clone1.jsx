7c3c3f2af9eff567e480135af81bd369f10588de
ABRAHAM AHMED <120266994+21abraham@users.noreply.github.com>
2024-01-26 01:53:08 +0530

    work experience by abraham

--- src/pages/clone1.jsx
+++ src/pages/clone1.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom'
 import { gsap } from 'gsap';
 import {ScrollTrigger} from 'gsap/ScrollTrigger';
 import myphoto from '../images/myphoto.jpg'
-import coventry from '../images/coventryphoto.jpg'
+import coventry from '../videos/coventry.mp4'
 gsap.registerPlugin(ScrollTrigger);
 export default function Clone1() {
     const navigate = useNavigate();
@@ -148,6 +148,7 @@ export default function Clone1() {
 
 
   useEffect(() => {
+      window.scroll(0,0);
       animatecircle1();
     animatecircle2();
       mouseimage();
@@ -162,7 +163,7 @@ export default function Clone1() {
                 </div>
 
                 <div className="w-full h-full col-span-5"></div>
-                <div className="hidden lg:flex lg:gap-x-12 justify-center items-center col-span-2">
+                <div className="hidden lg:flex lg:gap-x-12 justify-center col-span-2 items-center">
                         {navigation.map((item) => (
                             <button
                                 id="navlist"
@@ -180,36 +181,80 @@ export default function Clone1() {
                             </button>
                         ))}
                     </div>
+
             </nav>
            <div className="w-full h-screen px-4 py-4">
         <div className="w-full h-full rounded-2xl relative overflow-hidden">
           <div id="circle1" className="blob w-[700px] h-[700px] -z-1 rounded-full absolute top-0 right-0 bg-gradient-to-br from-transparent via-[#666da5] to-transparent blur-3xl bg-opacity-80"></div>
-          <div id={"circle2"} className="blob w-[700px] h-[700px] -z-1 rounded-full absolute bottom-0 left-0 bg-gradient-to-br from-transparent via-[#cd506f] to-transparent blur-3xl opacity-80"></div>
+          <div id={"circle2"} className="blob w-[700px] h-[700px] -z-1 rounded-full absolute bottom-0 left-0 bg-gradient-to-tl from-transparent via-[#cd506f] to-transparent blur-3xl opacity-80"></div>
           <h1 className="w-full h-full flex items-center justify-center text-3xl">Code architect and design alchemist, I shape ideas into responsive, dynamic, and elegant web solutions.</h1>
+            <div className="w-52 h-12 absolute right-0 bottom-0">
+                <div className="w-full h-full grid grid-cols-3 items-center justify-center">
+                    <div className="w-full h-full flex justify-center items-center">
+                        <a href="https://www.linkedin.com/in/abraham-ahmed-129374229"><svg
+  xmlns="http://www.w3.org/2000/svg"
+  className="h-5 w-5"
+  fill="currentColor"
+  viewBox="0 0 24 24">
+  <path
+    d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
+</svg></a>
+                    </div>
+                    <div className="w-full h-full flex justify-center items-center">
+                        <a href="https://github.com/21abraham"><svg
+  xmlns="http://www.w3.org/2000/svg"
+  className="h-5 w-5"
+  fill="currentColor"
+  viewBox="0 0 24 24">
+  <path
+    d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+</svg></a>
+                    </div>
+                    <div className="w-full h-full flex justify-center items-center">
+                        <a href="https://www.instagram.com/_abraham.py/"><svg
+  xmlns="http://www.w3.org/2000/svg"
+  className="h-5 w-5"
+  fill="currentColor"
+  viewBox="0 0 24 24">
+  <path
+    d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+</svg></a>
+                    </div>
+                </div>
+            </div>
         </div>
 
       </div>
             <div className="w-full h-full flex relative px-64 py-52">
-                <div className="gird grid-rows-2 gap-y-2 w-full h-full">
-                    <div className="w-full h-full grid grid-cols-3">
-                        <div className="w-full h-full col-span-2"><img className="w-full h-full" src={coventry} alt=""/></div>
-                        <div className="w-full h-full"><h1>hello</h1></div>
-                    </div>
-                    <div className="w-full h-full grid grid-cols-3">
-                        <div className="w-full h-full col-span-2"><img className="w-full h-full" src={coventry} alt=""/></div>
-                        <div className="w-full h-full"><h1>hello world</h1></div>
+                <div className="gap-y-2 w-full h-full">
+                    <div className="w-full h-full gap-x-6 grid grid-cols-3">
+                        <div className="w-full h-full col-span-2"><video
+                    className="w-[960px] h-[540px] object-cover"
+                    autoPlay
+                    loop
+                    muted
+                    playsInline
+                >
+                    <source src={coventry} type="video/mp4" />
+                </video></div>
+                        <div className="w-full h-full grid grid-rows-8">
+                            <div className="w-full h-full"><h1 className="tracking-widest text-2xl">WORK EXPERIENCE</h1></div>
+                            <div className="w-full h-full row-span-3"><h1 className="text-3xl">Intern at<br/>Coventry University, England<br/>June-July 2023</h1></div>
+                            <div className="w-full h-full row-span-2"><h1 className="text-xl">Tech Stack:<br/>.Net Framework, C# Script,<br/>Unity Engine, and AI</h1></div>
+                            <div className="w-full h-full row-span-2 flex items-end"><h1 className="text-lg">Implementation of<br/>A-star algorithm for<br/>NPC AI bot</h1></div>
+                        </div>
                     </div>
                 </div>
         </div>
             <div className="scroll w-full h-full relative overflow-x-hidden">
                 <div className="contain w-[400vw] h-full flex">
-                    <div className="panel w-screen h-screen flex items-center justify-center"><h1 className="text-3xl">My</h1></div>
+                    <div className="panel w-screen h-screen flex items-center justify-center"><h1 className="text-3xl">My life my rules</h1></div>
                     <div className="panel w-screen h-screen flex items-center justify-center"><h1 className="text-3xl">Life</h1></div>
                     <div className="panel w-screen h-screen flex items-center justify-center"><h1 className="text-3xl">My</h1></div>
                     <div className="panel w-screen h-screen flex items-center justify-center"><h1 className="text-3xl">Roles</h1></div>
             </div>
             </div>
-            <div className="w-full h-screen bg-blue-400 relative"></div>
+            <div className="w-full h-screen bg-blue-400 relative">Global warning</div>
     </div>
     );
 }

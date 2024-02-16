8f72a2d558ade8ef7b730ae9d0a3ad7f0e295051
ABRAHAM AHMED <120266994+21abraham@users.noreply.github.com>
2024-02-16 17:28:14 +0530

    finally deploy now!!!

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         <div className="App">
             <Router>
                 <Routes>
-                    <Route path="/" element={<LandingPage />} />
+                    <Route path="/AAhmed/" element={<LandingPage />} />
                 </Routes>
             </Router>
         </div>

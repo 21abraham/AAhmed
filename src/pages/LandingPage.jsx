8f16d50128bcb835e20e054f0d88570413bee893
ABRAHAM AHMED <120266994+21abraham@users.noreply.github.com>
2024-02-16 23:53:22 +0530

    finally deploy now!!!

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -605,7 +605,7 @@ const changenow = () => {
                     </div>
                 </div>
             </div>
-            <div className="lg:mt-20 flex justify-center items-center w-full h-12 bg-purple-900">
+            <div className="bottom-0 lg:mt-20 flex justify-center items-center w-full h-12 bg-purple-900">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 mr-2 animate-pulse">
   <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 3v1.5M4.5 8.25H3m18 0h-1.5M4.5 12H3m18 0h-1.5m-15 3.75H3m18 0h-1.5M8.25 19.5V21M12 3v1.5m0 15V21m3.75-18v1.5m0 15V21m-9-1.5h10.5a2.25 2.25 0 0 0 2.25-2.25V6.75a2.25 2.25 0 0 0-2.25-2.25H6.75A2.25 2.25 0 0 0 4.5 6.75v10.5a2.25 2.25 0 0 0 2.25 2.25Zm.75-12h9v9h-9v-9Z" />
 </svg>
